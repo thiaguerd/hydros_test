c562305de45cca082b12915169d5ea6bd50dc03b
thiaguerd <mail@thiago.pro>
2020-11-23 01:11:02 -0500

    configure static files

--- hydros/settings.py
+++ hydros/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hydros/static')
+]
+
+
 AUTH_USER_MODEL = 'users.User'
 SOCIAL_PROVIDERS = {
     'google': {
