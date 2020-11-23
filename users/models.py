e19812acc67b173f236c88346e67f18c5ce08097
thiaguerd <mail@thiago.pro>
2020-11-22 23:42:02 -0500

    add model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class User(models.Model):
+    uid            = models.CharField(max_length=20)
+    email          = models.CharField(max_length=20, unique=True)
+    name           = models.CharField(max_length=100)
+    given_name     = models.CharField(max_length=100)
+    family_name    = models.CharField(max_length=100)
+    picture        = models.CharField(max_length=100)
+    token          = models.CharField(max_length=30)
+    verified_email = models.BooleanField(default=False)
+    locale         = models.CharField(max_length=4)
+    last_login     = models.DateTimeField(default=datetime.now)
